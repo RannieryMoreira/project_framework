2b47b16c934fb799c30b536ee65697930bb5d732
Ranniery Moreira <moreiraranni@gmail.com>
2023-09-09 16:53:22 -0300

    cria os arquivos default do projeto

---
+++ src/main/java/com/tcc/projeto_framework/ProjetoFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.projeto_framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/projeto_framework/ProjetoFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.projeto_framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
